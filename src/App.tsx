f76750c0958b45ad4fbe9b6d72cafa5ee8648fe4
Joseph Castillo <joseph.cast0226@gmail.com>
2024-07-28 11:20:25 -0700

    Add textfields for code input and output
    The output textfield only mirrors the input at the moment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [sourceCode, setSourceCode] = useState("")
 
   return (
     <>
       <Button variant="contained">Hello world</Button>
+      <div className="flex-container">
+        <TextField
+          id="code-input"
+          placeholder="Place source code here"
+          rows={6}
+          multiline
+          fullWidth
+          value={sourceCode}
+          onChange={e => {
+            setSourceCode(e.target.value);
+          }}
+        />
+        <TextField
+          id="code-output"
+          placeholder="Output"
+          rows={6}
+          multiline
+          fullWidth
+          InputProps={{
+            readOnly: true,
+          }}
+          value={sourceCode}
+        />
+      </div>
     </>
   )
 }
